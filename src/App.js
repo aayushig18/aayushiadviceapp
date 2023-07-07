6309e63780105d02ac39695c75a72be7992864c7
Aayushi Gupta <105118745+aayushig18@users.noreply.github.com>
2023-07-07 18:17:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
-class App extends React.Component{
-    state={  advice: '' };
+import Trans from './Trans';
 
-    componentDidMount(){
-        this.fetchAdvice();
-    }
-    fetchAdvice=()=>{
-        axios.get('https://api.adviceslip.com/advice')
-             .then((response)=> {
-                const { advice }=response.data.slip;
-                this.setState({ advice});
-                 console.log(advice);
-             }) 
-             .catch((error)=>{
-                 console.log(error);
-             });
-    }
-    render(){
-        const {advice}=this.state;
-        return(
-            <div className="app">
-                <div className="card">
-                    <h1 className="heading">{advice}</h1>
-                    <button className="button" onClick={this.fetchAdvice}>
-                        <span>GIVE ME ADVICE!</span>
-                    </button>
-                </div>
-            </div>
-            
-        );
-    }
+import './App.css';
+
+class App extends React.Component {
+ 
+  state = { advice: ''};
+componentDidMount(){
+  this.fetchAdvice();
+}
+fetchAdvice = () => {
+  axios.get('https://api.adviceslip.com/advice')
+
+   .then((response) =>{
+    const {advice} = response.data.slip;
+    this.setState({advice});
+console.log (advice);
+
+
+  })
+  .catch((error) =>{
+console.log(error);
+  });
+}
+ 
+  render() {
+    const { advice}=this.state;
+    return (
+      
+      <div className="app">
+         
+        <div className="card">
+                
+          <h1 className="heading" >{advice}</h1>
+          
+          <button className="button" onClick={this.fetchAdvice}>
+<span>GIVE ME ADVICE!</span>
+          </button>
+      <Trans/>
+        </div>
+         
+   
+      </div>
+      
+    );
+  }
 }
 
 export default App;
